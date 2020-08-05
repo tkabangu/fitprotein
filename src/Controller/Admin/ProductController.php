1527a8aee9a31d3ddd7585836fc8b7d9ae5ed98c
Tresor <tkabangu1@gmail.com>
2020-08-05 14:40:17 +0200

    correction CRUD

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -7,7 +7,12 @@ namespace App\Controller\Admin;
 use App\Entity\Product;
 use App\Form\Type\ProductType;
 use App\Repository\ProductRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
@@ -17,15 +22,20 @@ class ProductController extends AbstractController
 
      */
     private $repository;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
 
-    public function __construct(ProductRepository $repository)
+    public function __construct(ProductRepository $repository, EntityManagerInterface $em)
     {
         $this->repository = $repository;
+        $this->em = $em;
     }
 
     /**
-     * @Route("/admin", name="admin.product.index")
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/admin/product", name="admin.product.index")
+     * @return Response
      */
     public function index()
     {
@@ -35,17 +45,61 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/admin/{id}", name="admin.product.edit")
-     * @param Product $product
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/admin/product/create", name="admin.product.new")
      */
-    public function edit(Product $product)
+    public function new(Request $request)
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($product);
+            $this->em->flush();
+            $this->addFlash('success', 'Créé avec succès');
+            return $this->redirectToRoute('admin.product.index');
+        }
+
+        return $this->render('admin/product/new.html.twig', [
+            'product' => $product,
+            'form'     => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/{id}", name="admin.product.edit", methods="GET|POST")
+     * @param Product $product
+     * @param Request $request
+     * @return Response
+     */
+    public function edit(Product $product, Request $request)
     {
         $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            $this->addFlash('success', 'Modifié avec succès');
+            return $this->redirectToRoute('admin.product.index');
+        }
         return $this->render('admin/product/edit.html.twig', [
             'product' => $product,
             'form' => $form->createView()
 
         ]);
+    }
+
+    /**
+     * @Route("/admin/product/{id}", name="admin.product.delete", methods="DELETE")
+     * @param Product $product
+     * @return RedirectResponse
+     */
+    public function delete(Product $product, Request $request) {
+        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->get('_token'))) {
+            $this->em->remove($product);
+            $this->em->flush();
+            $this->addFlash('success', 'Supprimé avec succès');
+        }
+        return $this->redirectToRoute('admin.product.index');
     }
 }
